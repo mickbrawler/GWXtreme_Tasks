ceddf3f78d50b815e8f6d8bdf5154398418f5fa5
Michael Camilo <camilom1@montclair.edu>
2021-10-13 17:16:18 -0400

    Reworked MCCMC. Plotting/Analysis script may still be needed

--- multiple_eos_random_compare.py
+++ multiple_eos_random_compare.py
@@ -7,10 +7,12 @@ import os
 import json
 
 # list of eos GWXtreme can work with off the cuff
-GWX_list = ["BHF_BBB2","KDE0V","KDE0V1","SKOP","HQC18","SLY2","SLY230A",
-            "SKMP","RS","SK255","SLY9","APR4_EPP","SKI2","SKI4","SKI6",
-            "SK272","SKI3","SKI5","MPA1","MS1B_PP","MS1_PP","BBB2","AP4",
-            "MPA1","MS1b","MS1","SLY"]
+#GWX_list = ["BHF_BBB2","KDE0V","KDE0V1","SKOP","HQC18","SLY2","SLY230A",
+#            "SKMP","RS","SK255","SLY9","APR4_EPP","SKI2","SKI4","SKI6",
+#            "SK272","SKI3","SKI5","MPA1","MS1B_PP","MS1_PP","BBB2","AP4",
+#            "MPA1","MS1b","MS1","SLY"]
+
+GWX_list = ["SLY"]
 
 # list of eos paper found p0,g1,g2,g3 values for
 pap_list = ["PAL6","AP1","AP2","AP3","AP4","FPS","WFF1","WFF2","WFF3"
@@ -52,19 +54,19 @@ p_eos_val = {"PAL6":[33.380,2.227,2.189,2.159]
              ,"ALF1":[33.055,2.013,3.389,2.033]
              ,"ALF2":[33.616,4.070,2.411,1.890]
              ,"ALF3":[33.283,2.883,2.653,1.952]
-             ,"ALF4":[33.314,3.009,3.438,1.803]}
+             ,"ALF4":[33.314,3.009,3.438,1.803]
              ,"SLY":[33.384,3.005,2.988,2.851]}
 
 modsel = ems.Model_selection(posteriorFile="posterior_samples/posterior_samples_narrow_spin_prior.dat")
 
-def likelihood(p0,g1,g2,g3,min_mass,max_mass,N,target_lambdas)
+def likelihood(p0,g1,g2,g3,min_mass,max_mass,N,target_lambdas):
     """
     METHOD: Takes in the value of the random parameters p0,g1,g2,g3 and some 
     other necessities, uses them to produce the trial_lambdas, and compares
     them with the target_lambdas.
     
     PARAMETERS:
-    ___________ 
+    ===========
     p0 : pressure.
     g1 : adiabatic energy density indice 1.
     g2 : adiabatic energy density indice 2.
@@ -76,24 +78,25 @@ def likelihood(p0,g1,g2,g3,min_mass,max_mass,N,target_lambdas)
     OUTPUT: Returns the value of the r-squared.
     """
 
-    s, Lambdas, min_mass, max_mass = modsel.getEoSInterpFrom_piecewise(p0,g1,g2,g3) # had to change "From_piecewise" to now return Lambdas like the normal "getEoSInterp"
+    s, min_mass, max_mass = modsel.getEoSInterpFrom_piecewise(p0,g1,g2,g3)
     trial_masses = np.linspace(min_mass,max_mass,N)
     trial_Lambdas = s(trial_masses)
-    trial_lambdas = (Lambdas / lal.G_SI) * ((trial_masses * lal.MRSUN_SI) ** 5) # lambdas the eos produced via getEosInterpFrom_piecewise.
+    trial_lambdas = (trial_Lambdas / lal.G_SI) * ((trial_masses * lal.MRSUN_SI) ** 5) # lambdas the eos produced via getEosInterpFrom_piecewise.
     r_val = 1 / np.log(np.sum((target_lambdas - trial_lambdas) ** 2)) # r^2 value
            
     return(r_val)
 
-def get_eos_parameter_dist(transitions,N):
+def get_eos_parameter_dist(transitions,N,filename):
     """
     METHOD: Compares the trial_lambdas resulting from different combinations of
     parameters with the target_lambdas for a certain eos. This is done for
     eos.
 
     PARAMETERS:
-    ___________
+    ===========
     transitions : The number of "states" changed. Each state is a parameter combination.
     N : The array size of the mass arrays.
+    filename : The name of the json file holding data dictionary
 
     OUTPUT: Creates a json holding the the parameter distributions for each eos.
     """
@@ -102,6 +105,7 @@ def get_eos_parameter_dist(transitions,N):
     eos_post_g1 = []
     eos_post_g2 = []
     eos_post_g3 = []
+    eos_post_r2 = []
 
     for eos in GWX_list: # we want to find the p0,g1,g2,g3 values to each GWXtreme eos
         
@@ -113,54 +117,55 @@ def get_eos_parameter_dist(transitions,N):
 
             log_p0_SI,g1,g2,g3 = 33.000,2.500,2.500,2.500
 
-        # range and step size of parameter space is hardcoded for now (easier to read)
-        p0_range = np.arange(log_p0_SI-.5,log_p0_SI+.5,.001)
-        g1_range = np.arange(g1-1.5,g1+1.5,.001)
-        g2_range = np.arange(g2-1.5,g2+1.5,.001)
-        g3_range = np.arange(g3-1.5,g3+1.5,.001)
-
         eos_pointer = lalsim.SimNeutronStarEOSByName(eos)
         fam_pointer = lalsim.CreateSimNeutronStarFamily(eos_pointer)
         min_mass = lalsim.SimNeutronStarFamMinimumMass(fam_pointer)/lal.MSUN_SI
 
-        s, _, Lambdas, max_mass = modsel.getEoSInterp(eosname=eos, m_min=min_mass)
+        s, _, _, max_mass = modsel.getEoSInterp(eosname=eos, m_min=min_mass)
         target_masses = np.linspace(min_mass,max_mass,N)
         target_Lambdas = s(target_masses)
-        target_lambdas = (Lambdas / lal.G_SI) * ((target_masses * lal.MRSUN_SI) ** 5) # essential to r^2 calculation
+        target_lambdas = (target_Lambdas / lal.G_SI) * ((target_masses * lal.MRSUN_SI) ** 5) # essential to r^2 calculation
 
         # METROPOLIS-HASTINGS 
         
-        no_errors == False
+        no_errors = False
 
         while no_errors == False:
 
-            p0_choice1, g1_choice1, g2_choice1, g3_choice1 = (np.random.choice(p0_range), np.random.choice(g1_range)
-                                                             ,np.random.choice(g2_range), np.random.choice(g3_range))
+            p0_choice1 = (log_p0_SI + np.random.random()) # increment is .5 so .5 * 2 is 1
+            g1_choice1 = (g1 + (3 * np.random.random())) # increment is 1.5
+            g2_choice1 = (g2 + (3 * np.random.random()))
+            g3_choice1 = (g3 + (3 * np.random.random()))
 
             try: 
 
                 L1 = likelihood(p0_choice1,g1_choice1,g2_choice1,g3_choice1,min_mass,max_mass,N,target_lambdas)
                 no_errors = True # if L1 doesn't give an error, the while loop will end
+                print("worked1")
 
-            # Can run into RuntimeError from (don't remember)
             # Can run into ValueError from the use of lal's piecewise function (I think)
-            except RuntimeError: continue
             except ValueError: continue
+            except RuntimeError: continue
 
         post_p0 = []
         post_g1 = []
         post_g2 = []
         post_g3 = []
+        post_r2 = []
         
-        while len(post_p0) <= transitions:
+        while len(post_p0) <= (transitions-1):
 
-            p0_choice2, g1_choice2, g2_choice2, g3_choice2 = (np.random.choice(p0_range), np.random.choice(g1_range)
-                                                             ,np.random.choice(g2_range), np.random.choice(g3_range))
+            p0_choice2 = (log_p0_SI + np.random.random()) # increment is .5 so .5 * 2 is 1
+            g1_choice2 = (g1 + (3 * np.random.random()))
+            g2_choice2 = (g2 + (3 * np.random.random()))
+            g3_choice2 = (g3 + (3 * np.random.random()))
 
-            try: L2 = likelihood(p0_choice2,g1_choice2,g2_choice2,g3_choice2,min_mass,max_mass,N,target_lambdas) # if L2 gives an error it'll keep trying
+            try: 
+                L2 = likelihood(p0_choice2,g1_choice2,g2_choice2,g3_choice2,min_mass,max_mass,N,target_lambdas) # if L2 gives an error it'll keep trying
+                print("worked2")
 
-            except RuntimeError: continue
             except ValueError: continue
+            except RuntimeError: continue
 
             if L2/L1 >= np.random.random():
 
@@ -168,6 +173,12 @@ def get_eos_parameter_dist(transitions,N):
                 g1_choice1 = g1_choice2
                 g2_choice1 = g2_choice2
                 g3_choice1 = g3_choice2
+
+                post_r2.append(L2) # if choice2s are better, append their likelihood
+                
+            else:
+
+                post_r2.append(L1) # otherwise choice1s are better, so their likelihood is appended instead
             
             # current eos' p0,g1,g2,g3 combination is stored (can then see what parameter combinations lasts the "longest")
             post_p0.append(p0_choice1)
@@ -180,8 +191,9 @@ def get_eos_parameter_dist(transitions,N):
         eos_post_g1.append(post_g1)
         eos_post_g2.append(post_g2)
         eos_post_g3.append(post_g3)
+        eos_post_r2.append(post_r2)
 
-data = {"p0" : eos_post_p0, "g1" : eos_post_g1, "g2" : eos_post_g2, "g3" : eos_post_g3}
-outputfile = "results/{}.json".format(filename)
-with open(outputfile, "w") as f:
-    json.dump(data, f, indent=2, sort_keys=True)
+    data = {"p0" : eos_post_p0, "g1" : eos_post_g1, "g2" : eos_post_g2, "g3" : eos_post_g3, "r2" : eos_post_r2}
+    outputfile = "results/{}.json".format(filename)
+    with open(outputfile, "w") as f:
+        json.dump(data, f, indent=2, sort_keys=True)

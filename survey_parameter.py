6bde3535f75367e7759c3523c081cc03a6634c0e
Michael Camilo <camilom1@montclair.edu>
2021-12-08 12:54:50 -0500

    Finished testing parameter range boundaries. Parameter survey begins.

---
+++ survey_parameter.py
@@ -0,0 +1,6 @@
+# Survey piecewise polytropic parameters. Fix three parameters. Loop over one 
+# of the parameters. See how fast the value of the evidence changes. This is 
+# called Profiling. This way we get a good idea of what are resolution should 
+# be for each parameter.
+
+

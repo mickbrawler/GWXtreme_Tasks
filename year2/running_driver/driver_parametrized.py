66c23528714be75cbe92b24d3b74841db9d6d02c
Michael Camilo <camilom1@montclair.edu>
2022-06-28 18:01:00 -0400

    Edited driver directories.

--- year2/running_driver/driver_parametrized.py
+++ year2/running_driver/driver_parametrized.py
@@ -4,13 +4,12 @@ import glob
 
 #Array Containing list of paths to the .dat files  containing the posterior samples for the events:
 
-paths = np.array(glob.glob("galpop/APR4_EPP/*"))
+paths = np.array(glob.glob("dat_files/galpop/APR4_EPP/*"))
 files = np.repeat("/bns_example_samples.dat", len(paths))
 fnames = list(np.char.add(paths,files))
 
 #Name of/ Path to file in which EoS parameter posterior samples will be saved:
 outname='Ap4_O3_injections'
-
 
 #Initialize Sampler Object:
 
@@ -23,7 +22,7 @@ outname='Ap4_O3_injections'
 
 """For Piece wise polytrope"""
 
-sampler=mcmc_sampler(fnames, {'logP':{'params':{"min":33.6,"max":34.5}},'gamma1':{'params':{"min":2.0,"max":4.5}},'gamma2':{'params':{"min":1.1,"max":4.5}},'gamma3':{'params':{"min":1.1,"max":4.5}}}, outname, nwalkers=100, Nsamples=10000, ndim=4, spectral=False, npool=100)
+sampler=mcmc_sampler(fnames, {'logP':{'params':{"min":33.6,"max":34.5}},'gamma1':{'params':{"min":2.0,"max":4.5}},'gamma2':{'params':{"min":1.1,"max":4.5}},'gamma3':{'params':{"min":1.1,"max":4.5}}}, outname, nwalkers=100, Nsamples=100, ndim=4, spectral=False, npool=100)
 
 #Run, Save , Plot
 
@@ -34,7 +33,4 @@ sampler.save_data()
 fig=sampler.plot(cornerplot={'plot':True,'true vals':None},p_vs_rho={'plot':True,'true_eos':'AP4'})
 fig['corner'].savefig('corner4_O3.png')
 fig['p_vs_rho'][0].savefig('eos4_O3.png')
-
-
-
 

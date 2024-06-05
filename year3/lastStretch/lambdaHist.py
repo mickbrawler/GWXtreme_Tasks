59ad06b266d76f7c086569c6aff910f05425e2e5
Michael Camilo <camilom1@montclair.edu>
2024-06-05 15:35:27 -0400

    Lambda(1.4) histogram plot logic is complete. Time to perfect it!

--- year3/lastStretch/lambdaHist.py
+++ year3/lastStretch/lambdaHist.py
@@ -3,18 +3,11 @@ import lal
 import lalsimulation as lalsim
 import matplotlib.pyplot as plt
 
-def getLambda_parametrized():
-    # Recycled code from GWXtreme.
-    
-    plt.figure(figsize=(12,12))
-    plt.rc('font', size=20)
-    plt.rc('axes', facecolor='#E6E6E6', edgecolor='black')
-    plt.rc('xtick', direction='out', color='black')
-    plt.rc('ytick', direction='out', color='black')
-    plt.rc('lines', linewidth=2)
+labels = ["2D-KDE-TaylorF2", "3D-KDE-TaylorF2", "3D-KDE-PhenomNRT"]
 
-    labels = ["2D-KDE-TaylorF2", "3D-KDE-TaylorF2", "3D-KDE-PhenomNRT"]
-    Labels = ["2D KDE TaylorF2", "3D KDE TaylorF2", "3D KDE PhenomNRT"]
+def calcLambda_parametrized():
+    # Recycled code from GWXtreme.
+
     for ii in range(len(labels)):
 
         filename='data/constraints/{}_16simulationsInference1000samp_gammas.txt'.format(labels[ii])
@@ -33,11 +26,37 @@ def getLambda_parametrized():
             Lambda = (2/3)*kk/(cc**5)
             Lambdas.append(Lambda)
 
-        plt.hist(Lambdas, label=Labels[ii])
+        np.savetxt("data/lambdaHists/{}_16simulationsInference1000samp_Lambdas.txt".format(labels[ii]),np.array(Lambdas).T)
 
-    plt.xlabel("r'$\Lambda$(1.4)",fontsize=20)
+
+def plotLambda_parametrized(eosname="APR4_EPP"):
+
+    m=1.4
+    eos = lalsim.SimNeutronStarEOSByName(eosname)
+    fam = lalsim.CreateSimNeutronStarFamily(eos)
+
+    rr = lalsim.SimNeutronStarRadius(m*lal.MSUN_SI, fam)
+    kk = lalsim.SimNeutronStarLoveNumberK2(m*lal.MSUN_SI, fam)
+    cc = m*lal.MRSUN_SI/rr
+    eosLambda = (2/3)*kk/(cc**5)
+
+    plt.figure(figsize=(12,12))
+    plt.rc('font', size=20)
+    plt.rc('axes', facecolor='#E6E6E6', edgecolor='black')
+    plt.rc('xtick', direction='out', color='black')
+    plt.rc('ytick', direction='out', color='black')
+    plt.rc('lines', linewidth=2)
+    Labels = ["2D KDE TaylorF2", "3D KDE TaylorF2", "3D KDE PhenomNRT"]
+    for ii in range(len(Labels)):
+
+        filename='data/lambdaHists/{}_16simulationsInference1000samp_Lambdas.txt'.format(labels[ii])
+        Lambdas = np.loadtxt(filename).T
+
+        plt.hist(Lambdas, label=Labels[ii], alpha=0.45, fill=False)
+
+    plt.axvline(x=eosLambda, label=eosname)
+
+    plt.xlabel("$\Lambda$(1.4)",fontsize=20)
+    plt.yticks([])
     plt.legend()
-
     plt.savefig("plots/lambdaHists/16simulations1000samp_Lambdas.png", bbox_inches='tight')
-
-#np.savetxt("data/constraints/{}_GW170817inference_gammas.txt".format(label),samples)

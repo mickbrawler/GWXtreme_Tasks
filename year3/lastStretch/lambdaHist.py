0d0f0583d1328f6f4058894e99d5933119b9679a
Michael Camilo <camilom1@montclair.edu>
2024-08-07 09:31:04 -0400

    lambda histograms redone.

--- year3/lastStretch/lambdaHist.py
+++ year3/lastStretch/lambdaHist.py
@@ -5,17 +5,16 @@ import matplotlib.pyplot as plt
 
 #labels = ["lalsim_nest-PhenomNRT", "2D-KDE-TaylorF2", "3D-KDE-TaylorF2", "3D-KDE-PhenomNRT"]
 #labels = ["2D-KDE-TaylorF2", "3D-KDE-TaylorF2", "3D-KDE-PhenomNRT"]
-#labels = ["lalsim_nest-PhenomNRT"]
-labels = ["3D-KDE-PhenomNRT"]
+labels = ["3D-KDE-PhenomPv2"]
 
 def calcLambda_parametrized():
     # Recycled code from GWXtreme.
 
     for ii in range(len(labels)):
 
+        filename='data/BNS/constraints/{}_GW170817inference_gammas.txt'.format(labels[ii])
         #filename='data/BNS/constraints/{}_16simulationsInference_gammas.txt'.format(labels[ii])
-        #filename='data/BNS/constraints/{}_GW170817inference_gammas.txt'.format(labels[ii])
-        filename='./data/NSBH/constraints/{}_18simulationsInference_1000samp_gammas.txt'.format(labels[ii])
+        #filename='./data/NSBH/constraints/{}_18simulationsInference_1000samp_gammas.txt'.format(labels[ii])
         samples = np.loadtxt(filename)
         Lambdas = []
         m=1.4
@@ -31,9 +30,9 @@ def calcLambda_parametrized():
             Lambda = (2/3)*kk/(cc**5)
             Lambdas.append(Lambda)
 
+        np.savetxt("data/BNS/lambdaHists/{}_GW170817inference_Lambdas.txt".format(labels[ii]),np.array(Lambdas).T)
         #np.savetxt("data/BNS/lambdaHists/{}_16simulationsInference_Lambdas.txt".format(labels[ii]),np.array(Lambdas).T)
-        #np.savetxt("data/BNS/lambdaHists/{}_GW170817inference_Lambdas.txt".format(labels[ii]),np.array(Lambdas).T)
-        np.savetxt("data/NSBH/lambdaHists/{}_18simulationsInference_1000samp_Lambdas.txt".format(labels[ii]),np.array(Lambdas).T)
+        #np.savetxt("data/NSBH/lambdaHists/{}_18simulationsInference_1000samp_Lambdas.txt".format(labels[ii]),np.array(Lambdas).T)
 
 
 
@@ -48,8 +47,9 @@ def plotLambda_parametrized(eosname="APR4_EPP"):
     cc = m*lal.MRSUN_SI/rr
     eosLambda = (2/3)*kk/(cc**5)
 
-    #Colors = ['#e41a1c','#377eb8','#4daf4a','#984ea3']
-    Colors = ['#e41a1c']
+    #Colors = ['#8dd3c7','#ffffb3','#bebada','#fb8072']
+    #Colors = ['#ffffb3','#bebada','#fb8072']
+    Colors = ['#fb8072']
 
     plt.figure(figsize=(12,12))
     plt.rc('font', size=20)
@@ -62,8 +62,8 @@ def plotLambda_parametrized(eosname="APR4_EPP"):
     Labels = ["3D KDE PhenomPv2"]
     for ii in range(len(Labels)):
 
-        #filename='data/BNS/lambdaHists/{}_16simulationsInference_Lambdas.txt'.format(labels[ii])
         #filename='data/BNS/lambdaHists/{}_GW170817inference_Lambdas.txt'.format(labels[ii])
+        #filename='data/BNS/lambdaHists/{}_16simulationsInference_Lambdas.txt'.format(labels[ii])
         filename='data/NSBH/lambdaHists/{}_18simulationsInference_1000samp_Lambdas.txt'.format(labels[ii])
         Lambdas = np.loadtxt(filename).T
         plt.hist(Lambdas, label=Labels[ii], alpha=0.45, fill=True, density=True, color=Colors[ii], histtype='step')
@@ -73,7 +73,7 @@ def plotLambda_parametrized(eosname="APR4_EPP"):
     plt.xlabel("$\Lambda$(1.4)",fontsize=20)
     plt.yticks([])
     plt.legend()
-    #plt.savefig("plots/lambdaHists/16simulations_Lambdas.png", bbox_inches='tight')
-    #plt.savefig("plots/lambdaHists/GW170817_Lambdas.png", bbox_inches='tight')
+    #plt.savefig("plots/BNS/lambdaHists/GW170817_Lambdas.png", bbox_inches='tight')
+    #plt.savefig("plots/BNS/lambdaHists/16simulations_Lambdas.png", bbox_inches='tight')
     plt.savefig("plots/NSBH/lambdaHists/18simulations_1000samp_Lambdas.png", bbox_inches='tight')
 

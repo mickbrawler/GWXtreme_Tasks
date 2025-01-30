cac912dce8e7c49c10e0448b814dc08173444d4d
Michael Camilo <camilom1@montclair.edu>
2025-01-30 13:17:59 -0500

    Did the lambdaT study Shaon did for the OG GWXtreme paper way back.... I think.

--- year3/lastStretch/lambdaReflectionStudy/reflectionTests.py
+++ year3/lastStretch/lambdaReflectionStudy/reflectionTests.py
@@ -6,27 +6,25 @@ from GWXtreme import eos_model_selection as ems
 import scipy.stats
 from sklearn.neighbors import KernelDensity
 
-# There's three ways to test if lambda_1's (really any lambda) kde can and should be reflected
+# There's two ways to test if lambda_1's (really any lambda) kde can and should be reflected
 
 # LOAD Data!!!
 filename = "../files/NSBH/gw230529_phenom_lowSpin.json"
 with open(filename,"r") as f: data = json.load(f)['posterior']['content']
 
-modsel = ems.Model_selection(filename,kdedim=3,logq=True)
-margPostData = modsel.margPostData
+#modsel = ems.Model_selection(filename,kdedim=3,logq=True) #uncomment later
+#margPostData = modsel.margPostData #uncomment later
 
 def method12():
     # One is simply using the resample logic on GWXtreme
-    # Second is doing similar but including the logic in the bounded_3d_kde code that accounts for boundaries
 
     Lambda_1 = margPostData[:,0]
     kde = modsel.kde
     yhigh = modsel.yhigh
     logq = modsel.logq
     logyhigh = modsel.logyhigh
-    #new_margPostData = modsel.kde.resample(size=len(margPostData)).T
 
-
+    # logic from get_trials() that gets you "synthetic" data
     new_margPostData = np.array([])
     counter = 0
     while len(new_margPostData) < len(margPostData):
@@ -43,7 +41,7 @@ def method12():
     chosen = np.random.choice(indices, len(margPostData))
     new_margPostData = new_margPostData[chosen]
 
-
+    # parameter we care about for this study
     resampledLambda1 = new_margPostData[:,0]
 
     plt.clf()
@@ -53,15 +51,13 @@ def method12():
     plt.xlabel("Lambda 1")
     #plt.savefig("low_zero_method12.png")
     plt.savefig("low_neginf_method12.png")
-    # I think method 1 is already method 2 if kdedim=3. The only way to do method 1
-    # as I thought would be to remove the boundary logic... I see why Anarya was surprised.
-    # I had misunderstood that resample was part of an external functionality.
 
 def method3():
-    # Third is to compare the hist of lambda_1 to the pdf of the kde of the lambda_1 distribution
+    # Second is to compare the hist of lambda_1 to the pdf of the kde of the lambda_1 distribution
     
     bw = 0.75 # is used by documentation
     bw = len(margPostData)**(-1/6) # is used by GWXtreme
+    print("bw val: {}".format(bw))
     lambda_1 = data['lambda_1']
     lambda_1_plot = np.linspace(min(lambda_1),max(lambda_1),1000)
 
@@ -88,3 +84,42 @@ def method3():
     #plt.savefig("scilearn_method3.png")
 
 
+def twoD_kdeTest():
+    # Trying to do the same lambdat studying Ghosh did for GWXtreme's 2D kde development
+    # A sanity check of sorts.
+
+    # LOAD Data!!!
+    filename = "/home/michael/projects/eos/GWXtreme_Tasks/year3/LSAMP_Poster/anaryaShare/Files/posterior_samples_narrow_spin_prior_170817.dat"
+
+    modsel = ems.Model_selection(filename) 
+    margPostData = modsel.margPostData
+
+    LambdaT = margPostData[:,0]
+    kde = modsel.kde
+    yhigh = modsel.yhigh
+
+    # logic from get_trials() that gets you "synthetic" data
+    new_margPostData = np.array([])
+    counter = 0
+    while len(new_margPostData) < len(margPostData):
+        prune_adjust_factor = 1.1 + counter/10.
+        N_resample = int(len(margPostData)*prune_adjust_factor)
+        new_margPostData = kde.resample(size=N_resample).T
+        unphysical = (new_margPostData[:, 0] < 0) +\
+                     (new_margPostData[:, 1] > yhigh) +\
+                     (new_margPostData[:, 1] < 0)
+        new_margPostData = new_margPostData[~unphysical]
+        counter += 1
+    indices = np.arange(len(new_margPostData))
+    chosen = np.random.choice(indices, len(margPostData))
+    new_margPostData = new_margPostData[chosen]
+
+    # parameter we care about for this study
+    resampledLambdaT = new_margPostData[:,0]
+
+    plt.clf()
+    plt.hist(np.log10(LambdaT),density=True,color='red',alpha=0.25,label="LambdaT")
+    plt.hist(np.log10(resampledLambdaT),density=True,color='blue',alpha=0.25,label="resampled")
+    plt.legend()
+    plt.xlabel("LambdaT")
+    plt.savefig("GW170817_LambdaT_test.png")
